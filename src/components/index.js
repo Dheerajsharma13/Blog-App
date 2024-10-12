61da41f6f1f678bf5ef92d0db59a488c7ed9ee38
dheeraj sharma <dheeraj.sharmaa13@gmail.com>
2024-10-12 22:19:33 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,32 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from './Button.jsx';
+import Input from "./Input.jsx";
+import PostCard from './PostCard';
+import Select from './Select.jsx';
+import Login from "./Login.jsx";
+import PostForm from "./post-form/PostForm";
+import AuthLayout from './AuthLayout';
+import Signup from './Signup';
+import RTE from './RTE.jsx';
+ 
+
+
+export { Header,
+        Footer, 
+        Container,
+        Logo,
+        LogoutBtn ,
+        Button,
+        Input,
+        PostCard,
+        Select,
+        Login,
+        PostForm,
+        AuthLayout,
+        Signup, 
+        RTE
+    }
